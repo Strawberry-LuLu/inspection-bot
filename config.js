d5c2086ab301990954f1e2ab29c74b17dcb02257
Yana <106078240+Strawberry-LuLu@users.noreply.github.com>
2026-07-03 17:22:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
+export const CONFIG = {
+  BOT_TOKEN: "",
+  SPREADSHEET_ID: "",
+  GOOGLE_SERVICE_EMAIL: "",
+  GOOGLE_PRIVATE_KEY: "",
 
+  BROKERS: [
+    "Артем Маркезини",
+    "Алина Мержоева",
+    "Виктория Азнаурян",
+    "Святослав Кукушкин",
+    "Олег Кривоносов",
+    "Любовь Коровкина",
+    "Елена Ахтареева"
+  ],
+
+  OBJECT_STATUSES: {
+    DRAFT: "Черновик",
+    READY: "Готов",
+    SENT: "Отправлен"
+  }
+};
