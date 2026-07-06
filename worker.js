ab4912ebdbf44785fd3e01b208b8e43961573764
Yana <106078240+Strawberry-LuLu@users.noreply.github.com>
2026-07-06 13:10:37 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,5 @@
+import { PDFDocument, rgb } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
 export default {
   async fetch(request, env, ctx) {
     return new Response("Bot is running");
